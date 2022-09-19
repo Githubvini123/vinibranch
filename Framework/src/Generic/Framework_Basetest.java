f3f2f000b5157dc48c55e291b513d6402f8396fd
vinay <vinay@VinayN>
2022-09-19 16:50:54 +0530

    first commit done

---
+++ Framework/src/Generic/Framework_Basetest.java
@@ -0,0 +1,13 @@
+package Generic;
+
+public interface Framework_Basetest 
+{
+	String chrome_key="webdriver.chrome.driver";
+	String chrome_value="./software/chromedriver.exe";
+	
+	String gecko_key="webdriver.gecko.driver";
+	String gecko_value="./software/geckodriver.exe";
+	
+	String Base_url="www.amazon.com";
+
+}
